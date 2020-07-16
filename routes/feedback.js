b8d1548969f668b3471f6a0161c35d68dccd8477
umer1212 <54633279+umer1212@users.noreply.github.com>
2020-07-17 00:47:31 +0500

    commit

--- routes/feedback.js
+++ routes/feedback.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const FeedBack = require('../models/FeedBack');
 const router = express.Router();
 const multer = require('multer');
-var cloudinary = require("cloudinary");
+const cloudinary = require('cloudinary')
 
 var storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -15,14 +15,23 @@ var storage = multer.diskStorage({
   });
 var upload = multer({ storage: storage,limits:{fileSize:12582912} })
 
-router.post('/uploadPhoto',async (req,res,next)=>{
-  const result = await cloudinary.v2.uploader.upload(req.file.path)
-  if(result){
-    res.status(200).json({
-      'read':true,
-      'ImageURL':result.url
-    });
-  }
+router.post('/uploadPhoto',(req,res,next)=>{
+	try{
+    const result = await cloudinary.v2.uploader.upload(req.file.path)
+    if(result){
+      res.status(200).json({
+        'read':true,
+        'ImageURL':result.url
+      });
+    }
+	}
+	catch(e){
+		res.status(500).json({
+			'read':false,
+			'err':e
+		})
+		console.log(e);
+	}
 });
 
 router.post('/',(req,res,next)=>{
